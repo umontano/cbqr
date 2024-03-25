895bcb019385fefabbb46c6a900206aa9df06d5f
umontano <u.montano.j@gmail.com>
2024-03-24 18:26:35 -0600

    added make dir

--- create_repo.sh
+++ create_repo.sh
@@ -2,7 +2,13 @@
 token_github="$1"
 repo_name="$2"
 destination_dir=~/a
+local_repo_dir="$destination_dir/$repo_name"
 
+## CREATE DIRECTORY
+mkdir -p "$local_repo_dir"
+cd "$local_repo_dir" || exit 1
+
+## CREATE REPO. INSTRUCTIONS ACCORDING TO GITHUB
 echo "# $repo_name" >> README.md
 git init
 git add README.md
@@ -12,7 +18,7 @@ git remote add origin git@github.com:umontano/"$repo_name".git
 git push -u origin main
 
 ## ADDING CREDENTIALS
-git config --global --add safe.directory "$destination_dir/$repo_name"/
+git config --global --add safe.directory "$local_repo_dir"/
 git config --global user.name umontano
 git config --global user.email u.montano.j@gmail.com
 git remote set-url origin https://umontano:"$token_github"@github.com/umontano/"$repo_name".git
