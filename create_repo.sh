c9b23e1ba18ae53743d10d3b6719b5ecefc4891e
umontano <u.montano.j@gmail.com>
2024-03-24 20:04:44 -0600

    vim edited

--- create_repo.sh
+++ create_repo.sh
@@ -8,6 +8,14 @@ local_repo_dir="$destination_dir/$repo_name"
 mkdir -p "$local_repo_dir"
 cd "$local_repo_dir" || exit 1
 
+
+## ADDING CREDENTIALS
+git init
+git config --global --add safe.directory "$local_repo_dir"/
+git config --global user.name umontano
+git config --global user.email u.montano.j@gmail.com
+git remote set-url origin https://umontano:"$token_github"@github.com/umontano/"$repo_name".git
+
 ## CREATE REPO. INSTRUCTIONS ACCORDING TO GITHUB
 echo "# $repo_name" >> README.md
 git init
@@ -16,10 +24,3 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:umontano/"$repo_name".git
 git push -u origin main
-
-## ADDING CREDENTIALS
-git config --global --add safe.directory "$local_repo_dir"/
-git config --global user.name umontano
-git config --global user.email u.montano.j@gmail.com
-git remote set-url origin https://umontano:"$token_github"@github.com/umontano/"$repo_name".git
-
