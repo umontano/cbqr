51548d7b60b4b2cb165aa3c40ed82fd793f2b407
umontano <u.montano.j@gmail.com>
2024-03-25 19:50:36 -0600

    tvbox hist.sh added

--- hist.sh
+++ hist.sh
@@ -1,3 +1,16 @@
+CBQ_comandos_SPSS_lab_ChyC
+exit 0
+#COPY HUGO FILES
+ssh -p 8022 u0_a66@192.168.0.13 '
+cd ~/a/CBQ_*/;
+ls;
+pwd;
+git pull;
+'
+scp *.[Rtm]* ~/a/cbqr/;
+
+
+#
 #!/bin/sh
 date=$(date +%y%m)
 filename=tvhist"$date".txt
